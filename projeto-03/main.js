bf935aed7cba065311219cd405dd30476742c830
iagoclima22 <iagoclima22@gmail.com>
2025-05-28 01:37:39 -0300

    Create main.js

---
+++ projeto-03/main.js
@@ -0,0 +1,7 @@
+let firstName = prompt('Digite o seu primeiro nome: ')
+let lastName = prompt('Digite o seu sobrenome: ')
+let age = prompt('Digite a sua idade: ')
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
