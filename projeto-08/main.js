ae78167a4ed890ebb702ab530f3dd1204c14c036
iagoclima22 <iagoclima22@gmail.com>
2025-05-30 15:35:13 -0300

    Create main.js

---
+++ projeto-08/main.js
@@ -0,0 +1,20 @@
+let grade = prompt('Digite a nota do aluno: ')
+
+if (grade > 100){
+    alert('Nota inválida')
+}
+else if(grade >= 90){
+    alert('Aluno nota A')
+}
+else if(grade >= 80){
+    alert('Aluno nota B')
+}
+else if(grade >= 70){
+    alert('Aluno nota C')
+}
+else if(grade >= 60){
+    alert('Aluno nota D')
+}
+else{
+    alert('Aluno nota F')
+}
